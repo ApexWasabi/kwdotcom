c0952fae6bbc4b8356adc659d96508a32fcfce41
ApexWasabi <64237868+ApexWasabi@users.noreply.github.com>
2022-04-11 08:48:07 +0900

    ye

---
+++ main.js
@@ -0,0 +1,26 @@
+LIVE
+Seperation
+var Messenger = function(el){
+  'use strict';
+  var m = this;
+  
+  m.init = function(){
+    m.codeletters = "&#*+%?£@§$";
+    m.message = 0;
+    m.current_length = 0;
+    m.fadeBuffer = false;
+    m.messages = [
+      'Privacy.',
+      'Solitude.',
+      'Seclusion.',
+      'Seperation.'
+    ];
+    
+    setTimeout(m.animateIn, 100);
+  };
+  
+  m.generateRandomString = function(length){
+    var random_text = '';
+    while(random_text.length < length){
+      random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+    }
