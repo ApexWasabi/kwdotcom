63f763a07cdfe3e54b29a7726e5fe5c350bf99c1
ApexWasabi <64237868+ApexWasabi@users.noreply.github.com>
2022-04-11 15:35:25 +0900

    sa

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ var Messenger = function(el){
     m.codeletters = "&#*+%?£@§$";
     m.message = 0;
     m.current_length = 0;
-    m.fadeBuffer = true;
+    m.fadeBuffer = false;
     m.messages = [
       'Kai Watanabe.',
       'Japanese.',
       'xPrivacy.',
       'xBluelock Org.',
-      'I believe that privacy is a fundamental human right.'
+      'Privacy for all.'
     ];
     
     setTimeout(m.animateIn, 100);
